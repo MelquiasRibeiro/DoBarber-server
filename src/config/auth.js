88d5256d32134aa60fe136c3ffed21217f6c8d0f
MelquiasRibeiro <melquias10.mr@gmail.com>
2020-04-10 22:53:05 -0300

    feat: Session and jwt auth

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '3f4826f2ea541edfede382d218f3b24d',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
@@ -8,5 +9,7 @@ routes.get('/users/:id', UserController.show);
 routes.post('/users', UserController.store);
 routes.put('/users/:id', UserController.update);
 routes.delete('/users/:id', UserController.destroy);
+
+routes.post('/session', SessionController.store);
 
 export default routes;
