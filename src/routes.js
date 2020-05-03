b9c1286ba05b68d948438dadb98bcf8d12d8c8bf
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-05-03 16:25:55 -0300

    feat: sending url user avatar

--- src/routes.js
+++ src/routes.js
@@ -5,22 +5,23 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileControler from './app/controllers/FileController';
+import ProviderControler from './app/controllers/ProviderController';
 
 import auth from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
-
 routes.post('/session', SessionController.store);
 
 routes.use(auth);
+
+routes.get('/users', UserController.index);
 routes.get('/users/:id', UserController.show);
 routes.put('/users', auth, UserController.update);
 routes.delete('/users/', UserController.destroy);
+routes.get('/providers', ProviderControler.index);
 routes.post('/files', upload.single('file'), FileControler.store);
-// routes.post('/files', upload.single('file'), (req, res) => res.json(req.file));
 
 export default routes;
