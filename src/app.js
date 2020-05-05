528a6c6b5487ee46e517a381cdb339048e760ba7
Melquias Ribeiro <melquias10.mr@gmail.com>
2020-05-05 16:00:10 -0300

    feat: try exceptions

--- src/app.js
+++ src/app.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+import Youch from 'youch';
+
 import Routes from './routes';
 import './database';
+import sentyConfig from './config/sentry';
 
 class APP {
   constructor() {
     this.server = express();
+
+    Sentry.init(sentyConfig);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -20,6 +30,15 @@ class APP {
 
   routes() {
     this.server.use(Routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
